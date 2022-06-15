0bdf136b0c7f94cf6cab62cdc19c549ae2aa1ea6
Gayathri-Sivakumar <79824505+Gayathri-Sivakumar@users.noreply.github.com>
2022-06-15 23:51:35 +0530

    changed

--- controllers/signinController.js
+++ controllers/signinController.js
@@ -5,64 +5,49 @@ const { createJWT } = require("../utils/auth");
 const emailRegexp =
   /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
 exports.signin = async (req, res) => {
-  let { email, password } = req.body;
-  console.log(email);
-  console.log(password);
-  let errors = [];
-  if (!email) {
-    errors.push({ email: "required" });
-    console.log("email required");
-  }
-  if (!emailRegexp.test(email)) {
-    errors.push({ email: "invalid email" });
-    console.log(errors);
-  }
-  if (!password) {
-    errors.push({ passowrd: "required" });
-    console.log(errors);
-  }
-  if (errors.length > 0) {
-    return res.status(422).json({ errors: errors });
-  }
-  await User.findOne({ email: email })
-    .then((user) => {
-      console.log(user);
-      if (!user) {
-        console.log("user not found");
-        return res.status(404).json({
-          errors: [{ user: "not found" }],
-        });
-      } else {
-        bcrypt
-          .compare(password, user.password)
-          .then((isMatch) => {
-            if (!isMatch) {
-              console.log("password is incorrect");
-              return res
-                .status(400)
-                .json({ errors: [{ password: "incorrect" }] });
-            }
-            let access_token = createJWT(user.email, user._id, 3600);
-            jwt.verify(access_token, process.env.JWT_SECRET, (err, decoded) => {
-              if (err) {
-                console.log("error in token");
-                res.status(500).json({ erros: err });
-              }
-              if (decoded) {
-                return res.status(200).json({
-                  success: true,
-                  token: access_token,
-                  message: user,
-                });
-              }
-            });
-          })
-          .catch((err) => {
-            res.status(500).json({ erros: err });
-          });
-      }
-    })
-    .catch((err) => {
-      res.status(500).json({ erros: err });
+  const { email, password } = req.body;
+  // if (!email) {
+  //   errors.push({ email: "required" });
+  //   console.log("email required");
+  // }
+  // if (!emailRegexp.test(email)) {
+  //   errors.push({ email: "invalid email" });
+  //   console.log(errors);
+  // }
+  // if (!password) {
+  //   errors.push({ passowrd: "required" });
+  //   console.log(errors);
+  // }
+  // if (errors.length > 0) {
+  //   return res.status(422).json({ errors: errors });
+  // }
+
+  const user = await User.findOne({ email });
+  // const id = await user._id;
+
+  if (!user)
+    return res.json({
+      success: false,
+      message: "user not found, with the given email!",
     });
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch)
+    return res.json({
+      success: false,
+      message: "email / password does not match!",
+    });
+
+  const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+    expiresIn: "1d",
+  });
+
+  const userInfo = {
+    fullname: user.fullname,
+    email: user.email,
+    avatar: user.avatar ? user.avatar : "",
+    id: user._id,
+  };
+
+  res.json({ success: true, user: userInfo, token });
 };
