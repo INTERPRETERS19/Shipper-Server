668c0f3089ed3c3427ae5968180cdca8e7ff6743
chambavy <chambavy@gmail.com>
2022-06-20 21:52:30 +0530

    signup_final_back

--- controllers/signupController.js
+++ controllers/signupController.js
@@ -1,38 +1,55 @@
 const JWT = require("jsonwebtoken");
-const User = require("../models/shipper");
+const Shipper = require("../models/shipper");
+// const Token = require("../models/Token.model");
+// const sendEmail = require("../utils/email/sendEmail");
+const crypto = require("crypto");
+const bcrypt = require("bcrypt");
+// const {
+//   emailVerification
+// } = require("../services/emailVerification.service");
+
 const JWTSecret = process.env.JWT_SECRET;
+const bcryptSalt = process.env.BCRYPT_SALT;
 
 exports.signup = async (req, res) => {
-  const { email, password, confirmPassword, firstName, lastName } = req.body;
-  //console.log(email);
+  const { email, password, firstName, lastName ,street, city, district, mobile_no} = req.body;
+  console.log(email);
 
   try {
-    const oldUser = await User.findOne({ email: email });
-
-    if (oldUser) throw new Error("Email already exist", 422);
-    if (password == confirmPassword) {
-      const result = await User.create({
-        email,
-        password,
-        firstName,
-        lastName,
-      });
-
-      const token = JWT.sign({ id: result._id }, JWTSecret);
-      data = {
-        userId: result._id,
-        email: result.email,
-        firstName: result.firstName,
-        token: token,
-      };
-      return res.status(200).json({
-        success: true,
-        token: token,
-        message: data,
+    const oldUser = await Shipper.findOne({ email: email });
+    if (oldUser) {
+      return res.json({
+        message: "email / password does not match!",
       });
     }
+    if (oldUser) throw new Error("Email already exist", 422);
+
+    const result = await Shipper.create({
+      email,
+      password,
+      firstName,
+      lastName,
+      street,
+      city,
+      district,
+      mobile_no
+    });
+
+    const token = JWT.sign({ id: result._id }, JWTSecret);
+    data = {
+      userId: result._id,
+      email: result.email,
+      firstName: result.firstName,
+      token: token,
+    };
+    return res.status(200).json({
+      success: true,
+      token: token,
+      message: data,
+    });
+
   } catch (error) {
-    res.status(500).json({ message: "Something went wrong" });
+    res.status(500).json({ message: "Something went wrong3" });
 
     console.log(error);
   }
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -6,6 +6,10 @@ const {
 
 const { signin } = require("../controllers/signinController");
 const { signup } = require("../controllers/signupController");
+const {
+  emailVerificationController,
+  requestEmailVerificationController
+} = require("../controllers/emailVerificationController");
 
 const router = require("express").Router();
 
@@ -14,5 +18,7 @@ router.post("/signin", signin);
 router.post("/requestResetPassword", resetPasswordRequestController);
 router.post("/resetPassword", resetPasswordController);
 router.post("/changePassword", changePasswordController);
+router.post("/emailVerification", emailVerificationController);
+router.post("/requestEmailVerification", requestEmailVerificationController);
 
 module.exports = router;
