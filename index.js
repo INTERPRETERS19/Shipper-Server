cd6703883cc60d72e07d253565be34781b45eea1
VaishnavyVS <vaishnasubram@gmail.com>
2022-06-28 15:21:50 +0530

    Qr code

--- index.js
+++ index.js
@@ -2,18 +2,19 @@ const http = require("http");
 require("express-async-errors");
 require("dotenv").config();
 
+const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
 const connection = require("./db");
 const cors = require("cors");
-// const port = 8080;
-
+const port = 8080;
 const useShipment = require("./routes/shipment");
 const useShipper = require("./routes/userRoute");
 const useBankDetails = require("./routes/bankdetails");
 const useHome = require("./routes/homeRouter");
-const useReview = require('./routes/review');
+// const imageRouter=require ('./routes/image');
+const useReview = require("./routes/review");
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -30,31 +31,26 @@ app.use(useShipment);
 app.use(useShipper);
 app.use(useBankDetails);
 app.use(useHome);
+// app.use('/image',imageRouter);
 app.use(useReview);
-
 app.use((error, req, res, next) => {
   res.status(500).json({ error: error.message });
 });
 
-// app.listen(port, () => {
-//   console.log("Listening to Port ", port);
-// });
-// app.use(express.static("../Shipper_Client/build"));
-
-// app.get("*", (req, res) => {
-//   res.sendFile(
-//     path.resolve(__dirname, "Shipper_Client", "build", "index.html")
-//   );
-// });
-http.createServer((request, response) => {
-  response.writeHead(200, { "Content-Type": "text/plain" });
-  response.end("Hello World!");
+app.listen(port, () => {
+  console.log("Listening to Port ", port);
 });
 
-const port = process.env.PORT || 8000;
-app.listen(port);
-
-// app.set("port", process.env.PORT || 8000);
-// console.log("App is listening" + app.get("port"));
-// response.end("Hello World!");
 module.exports = app;
+// // });
+// http.createServer((request, response) => {
+//   response.writeHead(200, { "Content-Type": "text/plain" });
+//   response.end("Hello World!");
+// });
+// const port = 8000;
+// // const port = process.env.PORT || 8000;
+// app.listen(port);
+
+// console.log(`App is listening ${port}`);
+// // response.end("Hello World!");
+// module.exports = app;
