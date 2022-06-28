be0cbd5b643416889c8ab50642d1c4a0bebea461
VaishnavyVS <vaishnasubram@gmail.com>
2022-06-28 21:28:29 +0530

    deploy 3

--- index.js
+++ index.js
@@ -14,12 +14,8 @@ const useShipper = require("./routes/userRoute");
 const useBankDetails = require("./routes/bankdetails");
 const useHome = require("./routes/homeRouter");
 // const imageRouter=require ('./routes/image');
-<<<<<<< HEAD
-const useReview = require("./routes/review");
-=======
 
-const useReview = require('./routes/review');
->>>>>>> 6c8985e3c23be8fb29c0296f255ed58d32b3f674
+const useReview = require("./routes/review");
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true }));
 
