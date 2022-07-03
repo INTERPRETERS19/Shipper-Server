527c8fb83b9fb81212418cce5865d5c29ed6caf7
Gayathri-Sivakumar <79824505+Gayathri-Sivakumar@users.noreply.github.com>
2022-07-03 14:55:48 +0530

    ok

--- models/shipment.js
+++ models/shipment.js
@@ -108,6 +108,11 @@ const ShipmentSchema = new mongoose.Schema({
     type: Date,
     default: null,
   },
+  delivery_fee: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
 });
 
 module.exports = mongoose.model("shipment", ShipmentSchema);
